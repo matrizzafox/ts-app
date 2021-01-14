c2c78c19b5f383a64cbb4abe9033383a7b23f573
Vasily Andrieieff <vas.matrizza@gmail.com>
2021-01-15 00:30:19 +0200

    added hithub link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,6 +6,7 @@ import './styles.css'
 const Header: React.FC = React.memo((): React.ReactElement => {
     return (
         <header className="header">
+            <p><b>GitHub:</b> <a target="__blank" href="https://github.com/matrizzafox/ts-app">github.com/matrizzafox/ts-app</a></p>
             <h1><i>TypeScript</i> TODO</h1>
             <ThemeToggle />
         </header>
