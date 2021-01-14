aa68e99a93bb8e5fbcdfc4c7c6c708a24b31cf2c
Vasily Andrieieff <vas.matrizza@gmail.com>
2021-01-15 01:04:31 +0200

    some fix

--- src/helpers/theme.ts
+++ src/helpers/theme.ts
@@ -6,9 +6,11 @@ export const setTheme = (theme: string): void => {
     localStorage.setItem("application-theme", theme)
 
     if (theme === "dark") {
+        document.querySelector("meta[name=theme-color]")?.setAttribute("content", '#222831')
         document.body.classList.add("dark")
         document.body.classList.remove("light")
     } else {
+        document.querySelector("meta[name=theme-color]")?.setAttribute("content", '#f5f5f5')
         document.body.classList.add("light")
         document.body.classList.remove("dark")
     }
